076aa3ae345985f2dadfb0afddb5c56521757017
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2022-01-29 18:47:36 +0100

    Fix: Position
    fix -b not working
    change int topbar and int centered to an enum of position

--- config.def.h
+++ config.def.h
@@ -1,8 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+typedef enum {
+	TOP,
+	CENTER,
+	BOTTOM
+} Position;
+
+static Position position = CENTER;          /* -c option; centers dmenu on screen */
 static int centered_height = 60;            /* if > 0: -c option will use centered_height as height value instead off centering on height too*/
 static int min_width = 700;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */

fbf40b6ce20cd5910ce3f93b00e469a25759e54e
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2022-01-31 14:55:00 +0100

    Apply Patch: symbols
    set symbols to FontAwesome chevrons.

--- dmenu.c
+++ dmenu.c
@@ -83,7 +83,7 @@ calcoffsets(void)
 	if (lines > 0)
 		n = lines * bh;
 	else
-		n = mw - (promptw + inputw + TEXTW("<") + TEXTW(">"));
+		n = mw - (promptw + inputw + TEXTW(symbol_left) + TEXTW(symbol_right));
 	/* calculate which items will begin the next page and previous page */
 	for (i = 0, next = curr; next; next = next->right)
 		if ((i += (lines > 0) ? bh : MIN(TEXTW(next->text), n)) > n)
@@ -219,18 +219,18 @@ drawmenu(void)
 	} else if (matches) {
 		/* draw horizontal list */
 		x += inputw;
-		w = TEXTW("<");
+		w = TEXTW(symbol_left);
 		if (curr->left) {
 			drw_setscheme(drw, scheme[SchemeNorm]);
-			drw_text(drw, x, 0, w, bh, lrpad / 2, "<", 0);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, symbol_left, 0);
 		}
 		x += w;
 		for (item = curr; item != next; item = item->right)
-			x = drawitem(item, x, 0, MIN(TEXTW(item->text), mw - x - TEXTW(">")));
+			x = drawitem(item, x, 0, MIN(TEXTW(item->text), mw - x - TEXTW(symbol_right)));
 		if (next) {
-			w = TEXTW(">");
+			w = TEXTW(symbol_right);
 			drw_setscheme(drw, scheme[SchemeNorm]);
-			drw_text(drw, mw - w, 0, w, bh, lrpad / 2, ">", 0);
+			drw_text(drw, mw - w, 0, w, bh, lrpad / 2, symbol_right, 0);
 		}
 	}
 	drw_map(drw, win, 0, 0, mw, mh, border_width);

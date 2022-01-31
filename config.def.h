c88d934b8df779a8eb1a0f3f337a4e041cf17553
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2022-01-31 14:55:00 +0100

    Apply Patch: symbols
    set symbols to FontAwesome chevrons.

--- config.def.h
+++ config.def.h
@@ -12,9 +12,12 @@ static int centered_height = 60;            /* if > 0: -c option will use center
 static int min_width = 700;                 /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"monospace:size=12",
+	"FontAwesome:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *symbol_left = "";
+static const char *symbol_right = "";
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },

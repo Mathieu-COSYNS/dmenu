8997b0e9c48edd5199586af1fcc31a829f736bb6
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2022-09-05 15:58:33 +0200

    Launch desktop apps based on app_info id

--- desktop_apps_launch.c
+++ desktop_apps_launch.c
@@ -15,7 +15,7 @@ int main(int argc, char const *argv[])
   while (app->next)
   {
     GAppInfo *app_info = app->data;
-    if (!strcmp(g_app_info_get_display_name(app_info), argv[1]))
+    if (!strcmp(g_app_info_get_id(app_info), argv[1]))
     {
       GError *error = NULL;
 
--- desktop_apps_list.c
+++ desktop_apps_list.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
   while (app->next)
   {
     GAppInfo *app_info = app->data;
-    printf("%s\n", g_app_info_get_display_name(app_info));
+    printf("%s %s\n", g_app_info_get_id(app_info), g_app_info_get_display_name(app_info));
     app = app->next;
   }
   return 0;
